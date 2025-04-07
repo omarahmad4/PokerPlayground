c2eefcdd9ad80584d0909834d096bcdaf56716ef
omarahmad4 <omar_farooq_ahmad@hotmail.com>
2025-04-07 14:44:50 -0400

    Initial Commit

---
+++ PokerPlayground/PokerPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerPlaygroundApp.swift
+//  PokerPlayground
+//
+//  Created by Omar Ahmad on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerPlaygroundUITests/PokerPlaygroundUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PokerPlaygroundUITestsLaunchTests.swift
+//  PokerPlaygroundUITests
+//
+//  Created by Omar Ahmad on 4/7/25.
+//
+
+import XCTest
+
+final class PokerPlaygroundUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
